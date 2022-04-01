9d2405a3c90d5c6ba39f82679b2ef7a0b4e68521
Josh Duncan <44387852+joshbduncan@users.noreply.github.com>
2022-04-01 16:01:20 -0400

    update ket test and precommit for new black

--- tests/test_WordSearch.py
+++ tests/test_WordSearch.py
@@ -15,7 +15,7 @@ def check_key(key: Key, puzzle: Puzzle) -> bool:
         row, col = info["start"]
         rmove, cmove = config.dir_moves[info["direction"]]
         for char in word:
-            if puzzle[row - 1][col - 1] != char:
+            if puzzle[row][col] != char:
                 return False
             row += rmove
             col += cmove
@@ -88,6 +88,8 @@ def test_bad_puzzle_size_type():
 
 def test_puzzle_key():
     puzzle = WordSearch(WORDS)
+    for r in puzzle.puzzle:
+        print(" ".join(r))
     assert check_key(puzzle.key, puzzle.puzzle)
 
 
